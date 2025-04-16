15858fc1a00a1131269f3e9979379199e1553311
aminabougallala <68965579+aminabougallala@users.noreply.github.com>
2025-04-16 10:21:28 -0500

    Create optimized_runner_loop.sh

---
+++ sample_packages/optimized_runner_loop.sh
@@ -0,0 +1,11 @@
+#Run pipelines in loop using cpdctl
+#!/bin/bash
+
+# Loop through all provided arguments
+for pipeline in "$@"; do
+    # Compile the pipeline for the current argument
+    time cpdctl dsjob compile-pipeline --project Project2021 --name "$pipeline"
+
+    # Run the pipeline for the current argument
+    time cpdctl dsjob run-pipeline --project Project2021 --name "$pipeline" --wait 3600 --optimize
+done
